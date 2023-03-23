a5126d1362b3f8e1659fdb46c1c8e722fa846677
ej-agas <agas.ej09@gmail.com>
2023-03-23 21:42:25 +0800

    inital commit
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let name = "world";
+  let src = "http://picsum.com/200/200";
+  let htmlString = "Some <strong>string</strong> with HTML";
+</script>
+
+<img {src} alt="Some text {name}" />
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html htmlString}</p>
+
+<style>
+  h1 {
+    color: magenta;
+  }
+</style>
