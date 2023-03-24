8d48625d819def0c4bfbd198c27b8d72491f529b
ej-agas <agas.ej09@gmail.com>
2023-03-24 22:58:03 +0800

    learn about slots
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
-
-  const props = {
-    initialCount: 5,
-    maxCount: 10,
-  }
-
+  import Button from './lib/Button.svelte';
 </script>
 
-<Counter {...props} />
+<Button>Svelte is Awesome</Button>
+
+<style>
+</style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<button><slot>FALLBACK</slot></button>
+
+<style>
+    button {
+        border: none;
+        background-color: #ff3e00;
+        color: #fff;
+        padding: 15px 20px;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
