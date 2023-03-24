7f05bf63e509ac479b8dd7df94af18e63b6bb913
ej-agas <agas.ej09@gmail.com>
2023-03-24 23:11:24 +0800

    learned about svelte preprocessor and sass
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -3,14 +3,17 @@
 
 <button><slot>FALLBACK</slot></button>
 
-<style>
+<style lang="scss">
     button {
         border: none;
-        background-color: #ff3e00;
-        color: #fff;
+        background-color: variables.$buttonColor;
+        color: variables.$buttonTextColor;
         padding: 15px 20px;
         font-weight: bold;
         border-radius: 5px;
         cursor: pointer;
+        &:hover {
+            background-color: variables.$buttonColorHover;
+        }
     }
 </style>
