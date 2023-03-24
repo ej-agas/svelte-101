7a8337339fb93d64f009527c5c280f709d922e03
ej-agas <agas.ej09@gmail.com>
2023-03-24 22:24:01 +0800

    learned about props
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import Counter from "./lib/Counter.svelte";
+
+  const props = {
+    initialCount: 5,
+    maxCount: 10,
+  }
+
 </script>
 
-<Counter />
+<Counter {...props} />
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
-  let count1 = 0;
-  let count2 = 0;
-  $: setCount2(count1);
-  $: string = `Count 2 is ${count2}`;
+  export let initialCount: number;
+  export let maxCount: number = undefined
 
-  function setCount2(value: number): void {
-    count2 = value;
-  }
+  let count: number = initialCount;
 
-  function increment1(): void {
-    count1 += 1;
-  }
-  function increment2(): void {
-    count2 += 1;
+  function increment() {
+    if (count < maxCount) {
+      count += 1;
+    }
   }
 </script>
 
-<button on:click={increment1}>Clicks {count1}</button>
-<button on:click={increment2}>Clicks {count2}</button>
-<h3>{string}</h3>
+<button on:click={increment}>Clicks {count}</button>
 
 <style>
   button {
