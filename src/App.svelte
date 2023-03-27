709acd531906de6b069dc7a3def4220134100275
ej-agas <agas.ej09@gmail.com>
2023-03-27 13:40:33 +0800

    learned practical uses for lifecycle events
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
     import TodoList from "./lib/TodoList.svelte";
     import { v4 as uuid } from "uuid";
     import type { Todo } from "./types";
-  import Button from "./lib/Button.svelte";
+    import Button from "./lib/Button.svelte";
 
     let todoList: TodoList;
     let showList: boolean = true;
@@ -10,49 +10,50 @@
     let todos: Map<string, Todo> = new Map();
 
     function addTodo(event: CustomEvent<{ text: string }>) {
-        event.preventDefault()
-        setTimeout(() => {
-            todos = new Map(todos)
-            todos.set(uuid(), {
-                id: uuid(),
-                text: event.detail.text,
-                done: false
-            })
-            todoList.clearInput()
-        }, 500)
+        event.preventDefault();
+        todos = new Map(todos);
+        const id = uuid();
+        todos.set(id, {
+            id: id,
+            text: event.detail.text,
+            done: false,
+        });
+        todoList.clearInput();
     }
 
     function deleteTodo(event: CustomEvent<{ id: string }>) {
-        todos = new Map(todos)
-        todos.delete(event.detail.id)
+        todos = new Map(todos);
+        todos.delete(event.detail.id);
+        console.log(event.detail.id);
+        console.table(todos);
     }
 
-    function toggleTodo(event: CustomEvent<{ id: string, done: boolean }>) {
-        todos = new Map(todos)
-        const todo = todos.get(event.detail.id)
+    function toggleTodo(event: CustomEvent<{ id: string; done: boolean }>) {
+        todos = new Map(todos);
+        const todo = todos.get(event.detail.id);
 
         if (!todo) return;
 
         todos.set(event.detail.id, {
             ...todo,
-            done: event.detail.done
-        })
+            done: event.detail.done,
+        });
     }
 </script>
 
 <label>
-    <input type="checkbox" bind:checked={showList}/>
+    <input type="checkbox" bind:checked="{showList}" />
     Show/Hide Todo List
 </label>
 {#if showList}
     <h1>{todos.size} Todos</h1>
     <TodoList
-        {todos}
-        bind:this={todoList}
-        on:addTodo={addTodo}
-        on:deleteTodo={deleteTodo}
-        on:toggleTodo={toggleTodo}
+        todos="{todos}"
+        bind:this="{todoList}"
+        on:addTodo="{addTodo}"
+        on:deleteTodo="{deleteTodo}"
+        on:toggleTodo="{toggleTodo}"
     />
 
-    <Button on:click={() => todoList.focusInput()}>Focus</Button>
+    <Button on:click="{() => todoList.focusInput()}">Focus</Button>
 {/if}
--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,24 +1,32 @@
-<svelte:options immutable={true} />
+<svelte:options immutable="{true}" />
 
 <script lang="ts">
     import type { Todo } from "src/types";
     import Button from "./Button.svelte";
-    import { createEventDispatcher, onDestroy, onMount } from "svelte";
+    import { createEventDispatcher, afterUpdate, beforeUpdate } from "svelte";
 
-    onMount(() => {
-        console.log('mounted')
-
-        return () => console.log('destroyed 2')
-    })
-    onDestroy(() => console.log('destroyed'))
+    afterUpdate(() => {
+        if (autoScroll) {
+            listDiv.scrollTop = listDiv.scrollHeight;
+        }
+        autoScroll = false;
+    });
 
     export let todos: Map<string, Todo> = new Map();
+    let previousTodos: Map<string, Todo> = todos;
+    let autoScroll: boolean = false;
 
-    let inputText: string = '';
+    let inputText: string = "";
     let input: HTMLInputElement;
+    let listDiv: HTMLDivElement;
+
+    $: {
+        autoScroll = todos.size > previousTodos.size;
+        previousTodos = todos;
+    }
 
     export function clearInput() {
-        inputText = '';
+        inputText = "";
     }
 
     export function focusInput() {
@@ -28,44 +36,55 @@
     const dispatch = createEventDispatcher();
 
     function addTodo() {
-        const isNotCancelled = dispatch('addTodo', { text: inputText }, { cancelable: true });
+        const isNotCancelled = dispatch(
+            "addTodo",
+            { text: inputText },
+            { cancelable: true }
+        );
 
         if (isNotCancelled) {
-            inputText = '';
+            inputText = "";
         }
     }
 
     function deleteTodo(id: string) {
-        dispatch('deleteTodo', { id: id });
+        dispatch("deleteTodo", { id: id });
     }
 
     function toggleTodo(id: string, done: boolean) {
-        dispatch('toggleTodo', { id: id, done: done });
+        dispatch("toggleTodo", { id: id, done: done });
     }
-
 </script>
 
 <div class="todo-list-wrapper">
-    <ul>
-        {#each [...todos] as [key, todo]}
-            <li>
-                <label for="">
-                    <input
-                        on:input={() => toggleTodo(todo.id, !todo.done)}
-                        type="checkbox"
-                        checked={todo.done}
+    <div class="todo-list" bind:this="{listDiv}">
+        <ul>
+            {#each [...todos] as [key, todo]}
+                <li>
+                    <label for="{todo.id}">
+                        <input
+                            on:input="{() => toggleTodo(todo.id, !todo.done)}"
+                            type="checkbox"
+                            checked="{todo.done}"
+                        />
+                        {todo.text}
+                    </label>
+                    <button on:click="{() => deleteTodo(todo.id)}"
+                        >Delete</button
                     >
-                    {todo.text}
-                </label>
-                <button on:click={() => deleteTodo(todo.id)}>Delete</button>
-            </li>
-        {/each}
-    </ul>
-    <form
-        on:submit|preventDefault={addTodo}
-        class="add-todo-form"
-    >
-    <input bind:this={input} bind:value={inputText}/>
-    <Button type="submit" disabled={!inputText}>Add</Button>
+                </li>
+            {/each}
+        </ul>
+    </div>
+    <form on:submit|preventDefault="{addTodo}" class="add-todo-form">
+        <input bind:this="{input}" bind:value="{inputText}" />
+        <Button type="submit" disabled="{!inputText}">Add</Button>
     </form>
 </div>
+
+<style>
+    .todo-list {
+        max-height: 150px;
+        overflow: auto;
+    }
+</style>
