5653ce642402d9be1f238b5e25afe792decf3852
ej-agas <agas.ej09@gmail.com>
2023-03-29 13:02:48 +0800

    learned about the `{#await}` block
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,31 @@
 
     let todos: Map<string, Todo> = new Map();
 
+    let todosPromise = loadTodos();
+
+    async function loadTodos(): Promise<Map<string, Todo>> {
+        let response = await fetch(
+            "https://jsonplaceholder.typicode.com/todos?_limit=10"
+        );
+
+        if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const json = await response.json();
+
+        todos = new Map(todos);
+        json.forEach((todo) => {
+            todos.set(todo.id, {
+                id: todo.id,
+                text: todo.title,
+                done: false,
+            });
+        });
+
+        return todos;
+    }
+
     async function addTodo(event: CustomEvent<{ text: string }>) {
         event.preventDefault();
         todos = new Map(todos);
@@ -48,12 +73,19 @@
     Show/Hide Todo List
 </label>
 {#if showList}
-    <h1>{todos.size} Todos</h1>
-    <TodoList
-        todos="{todos}"
-        bind:this="{todoList}"
-        on:addTodo="{addTodo}"
-        on:deleteTodo="{deleteTodo}"
-        on:toggleTodo="{toggleTodo}"
-    />
+    {#await todosPromise}
+        <p>Loading...</p>
+    {:then todos}
+        <h1>{todos.size} Todos</h1>
+        <TodoList
+            todos="{todos}"
+            bind:this="{todoList}"
+            on:addTodo="{addTodo}"
+            on:deleteTodo="{deleteTodo}"
+            on:toggleTodo="{toggleTodo}"
+        />
+    {:catch error}
+        <p>{error.message || "An error has occured"}</p>
+    {/await}
+    <button on:click="{() => (todosPromise = loadTodos())}">Refresh</button>
 {/if}
